c6adac024b3a6f6065e17451eda923b3d9cad19e
Natashi <32347635+Natashi@users.noreply.github.com>
2023-08-03 16:04:30 +0700

    AspHelpers

---
+++ DocumentsQA-Backend/Helpers/AspHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace DocumentsQA_Backend.Helpers {
+	// Just in case
+	// https://learn.microsoft.com/en-us/windows-server/identity/ad-fs/technical-reference/the-role-of-claims
+	public static class ClaimTypeURI {
+		public static readonly string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+		public static readonly string Name = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+		public static readonly string Role = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role";
+	}
+}
