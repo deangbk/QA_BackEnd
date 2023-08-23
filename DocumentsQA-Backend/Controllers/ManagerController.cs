e03c5e04c91cf149dc31851c84ed0deda5bbabef
Natashi <32347635+Natashi@users.noreply.github.com>
2023-08-23 11:21:47 +0700

    Add more docs, fix bugs, code cleanups

--- DocumentsQA-Backend/Controllers/ManagerController.cs
+++ DocumentsQA-Backend/Controllers/ManagerController.cs
@@ -42,12 +42,11 @@ namespace DocumentsQA_Backend.Controllers {
 		}
 
 		// -----------------------------------------------------
+
 		/// <summary>
-		/// sets tranche access for a user on a project, not managers
+		/// Grants tranche read access for a user to a project
+		/// <para>To grant management rights, see <see cref="AdminController.GrantProjectManagement"/></para>
 		/// </summary>
-		/// <param name="tid"></param>
-		/// <param name="uid"></param>
-		/// <returns></returns>
 		[HttpPut("grant_access/{tid}/{uid}")]
 		public async Task<IActionResult> GrantTrancheAccess(int tid, int uid) {
 			Tranche? tranche = await Queries.GetTrancheFromId(_dataContext, tid);
@@ -68,6 +67,20 @@ namespace DocumentsQA_Backend.Controllers {
 			var rows = await _dataContext.SaveChangesAsync();
 			return Ok(rows);
 		}
+
+		/// <summary>
+		/// Grants tranche read access for a group of users to a project
+		/// <para>To grant management rights, see <see cref="AdminController.GrantProjectManagementFromFile"/></para>
+		/// <example>
+		/// Example file structure is (newline is interpreted as a comma):
+		/// <code>
+		///		100, 101, 102
+		///		103
+		///		104, 105
+		///		110, 120, 1111
+		/// </code>
+		/// </example>
+		/// </summary>
 		[HttpPut("grant_access_withfile/{tid}")]
 		[RequestSizeLimit(bytes: 4 * 1024 * 1024)]  // 4MB
 		public async Task<IActionResult> GrantTrancheAccessFromFile(int tid, [FromForm] IFormFile file) {
@@ -96,8 +109,12 @@ namespace DocumentsQA_Backend.Controllers {
 			return Ok(rows);
 		}
 
+		/// <summary>
+		/// Removes user tranche read access
+		/// <para>To remove management rights, see <see cref="AdminController.RemoveProjectManagement"/></para>
+		/// </summary>
 		[HttpDelete("remove_access/{tid}/{uid}")]
-		public async Task<IActionResult> RemoveTrancheAccess(int tid, int uid, [FromQuery] bool elevated) {
+		public async Task<IActionResult> RemoveTrancheAccess(int tid, int uid) {
 			Tranche? tranche = await Queries.GetTrancheFromId(_dataContext, tid);
 			if (tranche == null)
 				return BadRequest("Tranche not found");
@@ -106,12 +123,11 @@ namespace DocumentsQA_Backend.Controllers {
 			if (!_access.AllowManageProject(project))
 				return Unauthorized();
 
-			if (!project.UserManagers.Exists(x => x.Id == uid)) {
-				tranche.UserAccesses.RemoveAll(x => x.Id == uid);
-			}
-			else {
+			if (project.UserManagers.Exists(x => x.Id == uid)) {
 				return BadRequest("Cannot remove access: user has elevated rights");
 			}
+
+			tranche.UserAccesses.RemoveAll(x => x.Id == uid);
 
 			var rows = await _dataContext.SaveChangesAsync();
 			return Ok(rows);
@@ -151,6 +167,22 @@ namespace DocumentsQA_Backend.Controllers {
 
 		// -----------------------------------------------------
 
+		/// <summary>
+		/// Creates new user in bulk, with access to specific tranches of a project
+		/// <para>Each line is a user data; email, display name [, tranches access...]</para>
+		/// <para>A user might also be created without any initial tranche access</para>
+		/// <para>Putting "*" as the tranche access will give access to all of the project's tranches</para>
+		/// <example>
+		/// Example file structure is:
+		/// <code>
+		///		aaaa@email.com, Maria, TrancheA
+		///		bbbb@email.com, hhhhhhhhhhh, TrancheA, TrancheB, TrancheC
+		///		cccc@email.com, M*rjorie T*ylor Gr*ene
+		///		dddd@email.com, asdkajfhsd, TrancheB
+		///		zzzz@fbc.us.gov, Jesse, *
+		/// </code>
+		/// </example>
+		/// </summary>
 		[HttpPost("add_project_users/{pid}")]
 		[RequestSizeLimit(bytes: 16 * 1024 * 1024)]  // 16MB
 		public async Task<IActionResult> AddUsersFromFile(int pid, [FromForm] IFormFile file) {
@@ -181,7 +213,7 @@ namespace DocumentsQA_Backend.Controllers {
 
 			Dictionary<string, int> trancheMap = project.Tranches
 				.ToDictionary(x => x.Name, x => x.Id);
-			List<(string email, string pass, string name, HashSet<int> tranches)> listUserData = new();
+			List<(string email, string pass, string name, HashSet<int>? tranches)> listUserData = new();
 
 			{
 				Random rnd = new Random(DateTime.Now.GetHashCode());
@@ -199,11 +231,19 @@ namespace DocumentsQA_Backend.Controllers {
 						string email = data[0];
 						string displayName = data[1];
 
-						HashSet<int> tranches = new();
+						HashSet<int>? tranches = null;
+
 						if (data.Length > 2) {
-							tranches = data[2].Split(',')
-								.Select(x => trancheMap[x])
-								.ToHashSet();
+							// * means all tranches, represented with a null, because this is unfortunately not Rust
+							if (data.Length == 3 && data[2] == "*") {
+								tranches = null;
+							}
+							else {
+								// Collect all tranches after as varargs-like
+								tranches = data.Skip(2)
+									.Select(x => trancheMap[x])
+									.ToHashSet();
+							}
 						}
 
 						string randPassword = _GeneratePassword(8);
@@ -213,37 +253,44 @@ namespace DocumentsQA_Backend.Controllers {
 					}
 				}
 				catch (Exception e) {
+					if (e is KeyNotFoundException) {
+						return BadRequest($"Tranche not found in project at line=\"{iLine}\"");
+					}
 					return BadRequest($"File parse error [line={iLine}]: " + e.Message);
 				}
 			}
 
 			List<AppUser> listUsers = new();
 			try {
+				// Wrap all operations in a transaction so failure would revert the entire thing
 				using (var transaction = _dataContext.Database.BeginTransaction()) {
 					// Warning: Inefficient
 					// If the system is to be scaled in the future, find some way to efficiently bulk-create users
 					//	rather than repeatedly awaiting CreateAsync
 
-					foreach (var i in listUserData) {
+					foreach (var (email, pass, name, _) in listUserData) {
 						var user = new AppUser {
-							Email = i.email,
-							UserName = i.email,
-							DisplayName = i.name,
+							Email = email,
+							UserName = email,
+							DisplayName = name,
 							Company = project.CompanyName,
 							FavouriteProjectId = project.Id,
 							DateCreated = dateCreated,
 						};
 						listUsers.Add(user);
 
-						var result = await _userManager.CreateAsync(user, i.pass);
+						var result = await _userManager.CreateAsync(user, pass);
 						if (!result.Succeeded)
-							throw new Exception(i.email);
+							throw new Exception(email);
 					}
 
 					foreach (var iTranche in project.Tranches) {
 						var accesses = listUsers
 							.Select((user, i) => (user, i))
-							.Where(x => listUserData[x.i].tranches.Contains(iTranche.Id))
+							.Where(x => {
+								var tranches = listUserData[x.i].tranches;
+								return tranches == null || tranches.Contains(iTranche.Id);
+							})
 							.Select(x => x.user)
 							.ToArray();
 						iTranche.UserAccesses.AddRange(accesses);
@@ -257,6 +304,7 @@ namespace DocumentsQA_Backend.Controllers {
 				return BadRequest("Users create failed: " + e.Message);
 			}
 
+			// Return IDs of all created users
 			var userIds = listUsers.Select(x => x.Id).ToList();
 
 			return Ok(userIds);
