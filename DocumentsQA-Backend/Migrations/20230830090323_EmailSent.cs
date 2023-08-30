c4eee168c082a6db787677fa9fcef7615dedee20
Natashi <32347635+Natashi@users.noreply.github.com>
2023-08-30 16:05:22 +0700

    Add migration

---
+++ DocumentsQA-Backend/Migrations/20230830090323_EmailSent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentsQA_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailSent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyEmailSent",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAnswered",
+                table: "Questions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastEmailSentDate",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAnswered",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "LastEmailSentDate",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DailyEmailSent",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- DocumentsQA-Backend/Models/Logs.cs
+++ DocumentsQA-Backend/Models/Logs.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
