b13de14df56f72c4f3f8e884a4f95c2be8f9a79d
Natashi <32347635+Natashi@users.noreply.github.com>
2024-02-19 16:24:40 +0700

    Relocate CreateProjectDTO

--- DocumentsQA-Backend/DTO/AdminDTO.cs
+++ DocumentsQA-Backend/DTO/AdminDTO.cs
@@ -6,18 +6,5 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocumentsQA_Backend.DTO {
-	// Required properties must be nullable otherwise they'll be default-initialized if missing when used with FromBody
-
-	public class CreateProjectDTO {
-		public string Name { get; set; } = null!;
-
-		public string Company { get; set; } = null!;
-
-		[Required] public DateTime? DateStart { get; set; }
-
-		[Required] public DateTime? DateEnd { get; set; }
-
-		[JsonPropertyName("tranches")]
-		public string InitialTranches { get; set; } = null!;
-	}
+	
 }
--- DocumentsQA-Backend/DTO/ProjectDTO.cs
+++ DocumentsQA-Backend/DTO/ProjectDTO.cs
@@ -6,6 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocumentsQA_Backend.DTO {
+	// Required properties must be nullable otherwise they'll be default-initialized if missing when used with FromBody
+
+	public class CreateProjectDTO {
+		public string Name { get; set; } = null!;
+
+		public string Company { get; set; } = null!;
+
+		[JsonPropertyName("date_start")]
+		[Required] public DateTime? DateStart { get; set; }
+
+		[JsonPropertyName("date_end")]
+		[Required] public DateTime? DateEnd { get; set; }
+
+		[JsonPropertyName("tranches")]
+		public string InitialTranches { get; set; } = null!;
+	}
+
 	public class AddNoteDTO {
 		public string Text { get; set; } = null!;
 
