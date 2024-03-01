29dd56db7c5193cb7fe0f5ce885b43422e02b5be
Natashi <32347635+Natashi@users.noreply.github.com>
2024-03-01 15:42:24 +0700

    Add AuthHelpers.GuardDetailsLevel

--- DocumentsQA-Backend/Helpers/AuthHelpers.cs
+++ DocumentsQA-Backend/Helpers/AuthHelpers.cs
@@ -9,8 +9,20 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
+using DocumentsQA_Backend.Models;
+using DocumentsQA_Backend.Services;
+
 namespace DocumentsQA_Backend.Helpers {
 	public static class AuthHelpers {
-		
+		/// <summary>
+		/// Throws an AccessForbiddenException if the requested details level is higher than the maximum allowed.
+		/// <para>Does nothing if the user has manager access.</para>
+		/// </summary>
+		public static void GuardDetailsLevel(IAccessService access, Project project, int details, int maxDetails) {
+			if (details >= maxDetails) {
+				if (!access.AllowManageProject(project))
+					throw new AccessForbiddenException($"Insufficient credentials to get details level {details}");
+			}
+		}
 	}
 }
---
+++ DocumentsQA-Backend/Helpers/DocumentHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using DocumentsQA_Backend.Models;
+using DocumentsQA_Backend.Data;
+using DocumentsQA_Backend.Services;
+
+namespace DocumentsQA_Backend.Helpers {
+	public static class DocumentHelpers {
+		
+	}
+}
