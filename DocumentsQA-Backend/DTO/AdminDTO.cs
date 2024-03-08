f3af49934cff6b70cf1362d99e48bd807e771c3e
Natashi <32347635+Natashi@users.noreply.github.com>
2024-03-08 15:40:38 +0700

    Update login to use projectId prefixing

--- DocumentsQA-Backend/DTO/AdminDTO.cs
+++ DocumentsQA-Backend/DTO/AdminDTO.cs
@@ -6,5 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocumentsQA_Backend.DTO {
-	
+	public class AddUserDTO {
+		public string Email { get; set; } = null!;
+		public string Name { get; set; } = null!;
+		public string? Company { get; set; }
+		public List<string>? Tranches { get; set; }
+	}
 }
--- DocumentsQA-Backend/Helpers/AuthHelpers.cs
+++ DocumentsQA-Backend/Helpers/AuthHelpers.cs
@@ -11,9 +11,28 @@ using Microsoft.AspNetCore.Identity;
 
 using DocumentsQA_Backend.Models;
 using DocumentsQA_Backend.Services;
+using DocumentsQA_Backend.Extensions;
 
 namespace DocumentsQA_Backend.Helpers {
 	public static class AuthHelpers {
+		public static string ComposeUsername(int projectId, string name) {
+			return $"{projectId}:{name}";
+		}
+		public static (int, string) DecomposeUsername(string name) {
+			int sep = name.IndexOf(':');
+			if (sep == -1) {
+				return (-1, "");
+			}
+
+			int project = -1;
+			try {
+				project = int.Parse(name[..sep]);
+			}
+			catch {}
+
+			return (project, name[(sep + 1)..]);
+		}
+
 		/// <summary>
 		/// Throws an AccessForbiddenException if the requested details level is higher than the maximum allowed.
 		/// <para>Does nothing if the user has manager access.</para>
