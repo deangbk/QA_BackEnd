a8729f9e88199aadc5e3ee3ee7f49d538b2c9b76
Natashi <32347635+Natashi@users.noreply.github.com>
2024-03-13 17:10:02 +0700

    Add DateSend database migration

---
+++ DocumentsQA-Backend/Migrations/20240313100612_DateSent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentsQA_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateSent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateSent",
+                table: "Questions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+			// Initialize DateSent to DatePosted
+			migrationBuilder.Sql(
+				@"
+					update [Questions]
+					set DateSent = DatePosted
+				");
+		}
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateSent",
+                table: "Questions");
+        }
+    }
+}
